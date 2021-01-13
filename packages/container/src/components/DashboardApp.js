b93cde12f382be6595a4352a738695b023f2f16f
Kirill Evtushenko <kirillzhoh@gmail.com>
2021-01-13 18:21:04 +0400

    feat: added basic vue app

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,13 @@
+import React, { useRef, useEffect } from "react";
+
+import { mount } from "dashboard/Dashboard";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
